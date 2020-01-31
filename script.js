50dd235f4ca6094e2a5e80f002915543f7efff92
pradeepdsmk <pradeepdsmk@gmail.com>
2020-01-31 22:33:23 +0530

    add / remove stock rows in trades table using buttons - ui only

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
 $(() => {
-    $('#trades').on('keyup', 'tr.buy input[name="quantity"]', (e) => {
-        onBuyQuantityUpdated(e.target)
-    })
+    
 })
 
-function onBuyQuantityUpdated(uiBuyQuantity) {
-    let quantity = parseInt($(uiBuyQuantity).val())
-    uiStock = uiBuyQuantity.closest('tbody');
-    updateSellQuantity(uiStock, quantity)
-    updateBuy(uiStock)
-    updateSell(uiStock)
-}
-
-function onGrossBuyUpdated() {
-
-}
-
-function onGrossSellUpdated() {
-
-}
-
-function updateSellQuantity(uiStock, quantity) {
-    uiStock.find('tr.sell input[name="quantity"]').val(quantity)
-}
-
-function updateBuy(uiStock) {
-    updateSummary(uiStock)
-}
-
-function updateSell(uiStock) {
-    updateSummary(uiStock)
-}
-
-function updateSummary(uiStock) {
-
-}
 
 class IdSeries {
     constructor(prefix) {
@@ -52,9 +19,8 @@ class IdSeries {
     }
 }
 
-class TradeRow {
-    constructor(tradeId) {
-        this._tradeId = tradeId
+class Stock {
+    constructor(uiStock) {        
         this._grossBuy = 0
         this._grossSell = 0
         this._quantity = 0
@@ -65,7 +31,8 @@ class TradeRow {
         this._totaySell = 0
         this._summary = 0
 
-        this.uiTbody = $()
+        this._uiStock = uiStock
+        this._stockId = this._uiStock.attr('id')
     }
 
     grossBuy(val) {
@@ -76,40 +43,118 @@ class TradeRow {
 
         }
     }
+
+    stockId() {
+        return this._stockId
+    }
+
+
+    onBuyQuantityUpdated(uiBuyQuantity) {
+        let quantity = parseInt($(uiBuyQuantity).val())
+        uiStock = uiBuyQuantity.closest('tbody');
+        updateSellQuantity(uiStock, quantity)
+        updateBuy(uiStock)
+        updateSell(uiStock)
+    }
+
+    onGrossBuyUpdated() {
+
+    }
+
+    onGrossSellUpdated() {
+
+    }
+
+    updateSellQuantity(uiStock, quantity) {
+        uiStock.find('tr.sell input[name="quantity"]').val(quantity)
+    }
+
+    updateBuy(uiStock) {
+        updateSummary(uiStock)
+    }
+
+    updateSell(uiStock) {
+        updateSummary(uiStock)
+    }
+
+    updateSummary(uiStock) {
+
+    }
 }
 
 
-
 var TradesTable = (function () {
-    var trades = []
-    var tradeIdSeries
+    var stocks
+    var stockIdSeries
     var uiTradesTable
-    var uiStockRowTemplate
+    var uiStockTemplate
 
     var init = function () {
-        trades = []
-        tradeIdSeries = new IdSeries('trade-row')
+        stocks = []
+        stockIdSeries = new IdSeries('trade-row')
         uiTradesTable = $('#trades')
-        let uiStockRow = $(uiTradesTable.find('tbody.stock').get(0))
-        uiStockRowTemplate = uiStockRow.clone()
-        uiStockRow.remove()
-        console.log(uiStockRowTemplate)
+
+        let uiStock = $(uiTradesTable.find('tbody.stock').get(0))
+        uiStockTemplate = uiStock.clone()
+        uiStock.remove()
+
+        bindUiActions()
+
+        addStock()
     }
 
-    var add = function () {
-        let tradeId = tradeIdSeries.next()
-        let tradeRow = new TradeRow(tradeId)
-        trades.push(tradeRow)
+    var bindUiActions = function () {
+        $('#add-stock').click(function (e) {
+            addStock()
+        })
 
-        uiTradesTable.append(uiStockRowTemplate.clone())
+        uiTradesTable.on('click', '.remove-stock', (e) => {
+            let uiStock = $(e.target).closest('tbody')
+            removeStock(uiStock)
+        })
+
+        uiTradesTable.on('keyup', 'tr.buy input[name="quantity"]', (e) => {
+            // onBuyQuantityUpdated(e.target)
+
+        })
+    }
+
+    var addStock = function () {
+        let stockId = stockIdSeries.next()
+
+        let uiStock = uiStockTemplate.clone()
+        uiStock.attr('id', stockId)
+
+        let stock = new Stock(uiStock)
+        stocks.push(stock)
+
+        uiTradesTable.append(uiStock)
+
+        console.log(stocks)
+    }
+
+    var removeStock = function (uiStock) {
+        let stockId = uiStock.attr('id')
+
+        for (let i = 0; i < stocks.length; i++) {
+            if (stocks[i].stockId() === stockId) {
+                stocks.splice(i, 1)
+                break
+            }
+        }
+
+        uiStock.remove()
+
+        console.log(stocks)
     }
 
     return {
-        init: init,
-        add: add
+        init: init
     }
 })()
 
-TradesTable.init()
-TradesTable.add()
-TradesTable.add()
+
+
+$(() => {
+    TradesTable.init()
+})
